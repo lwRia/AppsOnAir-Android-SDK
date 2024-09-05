ac35e388a72f05d5af2ff86028cfce60b77db86c
lwRia <riya@logicwind.com>
2024-09-05 14:10:42 +0530

    file provider package Confliction issue resolved

--- appsonair/src/main/java/com/appsonair/adapters/ShakeBugAdapter.java
+++ appsonair/src/main/java/com/appsonair/adapters/ShakeBugAdapter.java
@@ -34,10 +34,10 @@ public class ShakeBugAdapter extends RecyclerView.Adapter<ShakeBugAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull ShakeBugAdapter.ViewHolder holder, int position) {
         Uri imagePath = imageList.get(position);
-        // holder.imgBug.setImageURI(imagePath);
         Glide.with(holder.itemView.getContext())
                 .load(imagePath)
                 .into(holder.imgBug);
+        holder.imgBug.setClipToOutline(true);
     }
 
     @Override
